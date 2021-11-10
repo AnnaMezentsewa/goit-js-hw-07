88f72adf51afe9cec9247f281cc859afb59016aa
AnnaMezentsewa <annamezencewa@gmail.com>
2021-11-11 01:43:41 +0200

    решение через forEach

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,11 @@
-const categoryEl = document.querySelector('#categories').children;
+const categoryEl = document.querySelectorAll('.item');
 console.log(`В списке ${categoryEl.length} категории.`);
 
-const itemEl = document.querySelector(".item");
-const titleEl = itemEl.firstElementChild;
-console.log(`Категория: ${titleEl.textContent}`);
-console.log(`Количество элементов: ${itemEl.querySelectorAll("li").length}`);
 
-const secondEl = itemEl.nextElementSibling;
-console.log(`Категория: ${secondEl.firstElementChild.textContent}`);
-console.log(`Количество элементов: ${secondEl.querySelectorAll("li").length}`);
+const navItemEl = document.querySelectorAll('li.item');
+navItemEl.forEach((item => {
+    console.log(`Категория: ${item.firstElementChild.textContent}`);
+    console.log(`Количество элементов: ${item.lastElementChild.children.length}`);
+})
+)
 
-const thirdEl = secondEl.nextElementSibling;
-console.log(`Категория: ${thirdEl.firstElementChild.textContent}`);
-console.log(`Количество элементов: ${thirdEl.querySelectorAll("li").length}`);
